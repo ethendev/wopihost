8ecf8a44ac983ce8edbb18d6369f77908fad02b6
ethendev <ethanchan.se@gmail.com>
2017-04-18 09:53:23 +0800

    服务接口

---
+++ src/main/java/com/wopihost/WopihostApplication.java
@@ -0,0 +1,12 @@
+package com.wopihost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WopihostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WopihostApplication.class, args);
+	}
+}
